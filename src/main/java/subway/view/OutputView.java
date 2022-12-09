bea9078f6d155503e62b966fb4bf76473e6d0794
limheesang <dlagmltkd12@naver.com>
2022-12-09 17:05:22 +0900

    feat: 구간 등록, 삭제 결과 출력 기능

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -8,6 +8,8 @@ public class OutputView {
     private static final String INFO_DELETE_STATION_MESSAGE = "[INFO] 지하철 역이 삭제되었습니다.";
     private static final String INFO_CREATE_LINE_MESSAGE = "[INFO] 지하철 노선이 등록되었습니다.";
     private static final String INFO_DELETE_LINE_MESSAGE = "[INFO] 지하철 노선이 삭제되었습니다.";
+    private static final String INFO_CREATE_SECTION_MESSAGE = "[INFO] 구간이 등록되었습니다.";
+    private static final String INFO_DELETE_SECTION_MESSAGE = "[INFO] 구간이 삭제되었습니다.";
 
     private OutputView() {
     }
@@ -66,5 +68,13 @@ public class OutputView {
             System.out.println(sectionFeature.toString());
         }
         printBlank();
+    }
+
+    public static void printCreateSection() {
+        System.out.println(INFO_CREATE_SECTION_MESSAGE);
+    }
+
+    public static void printDeleteSection() {
+        System.out.println(INFO_DELETE_SECTION_MESSAGE);
     }
 }
